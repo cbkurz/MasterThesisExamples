5a02ae3cc3dbbbff607e6019a7c2b3b184696eca
Clemens Kurz <ClemensB.Kurz@gmail.com>
2023-08-30 20:07:18 +0200

    Kieker2Uml: * setup the teetime framework so it feeds the SequenceDiagrammFilter * this has been done with information from kieker.tools.trace.analysis.TraceAnalysisConfiguration

--- Kieker2Uml/src/main/java/org/kurz/ma/examples/kieker2uml/filter/SequenceDiagrammFilter.java
+++ Kieker2Uml/src/main/java/org/kurz/ma/examples/kieker2uml/filter/SequenceDiagrammFilter.java
@@ -6,21 +6,29 @@ import kieker.model.system.model.MessageTrace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+
 public class SequenceDiagrammFilter extends AbstractMessageTraceProcessingFilter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SequenceDiagrammFilter.class);
+    private final Path outputPath;
 
     /**
-     * Creates a new instance of this class using the given parameters.
-     *
      * @param repository model repository
+     * @param outputPath the path to the file to which the sequence diagramm is written.
      */
-    public SequenceDiagrammFilter(final SystemModelRepository repository) {
+    public SequenceDiagrammFilter(final SystemModelRepository repository, final Path outputPath) {
         super(repository);
+        this.outputPath = outputPath;
     }
 
     @Override
-    protected void execute(final MessageTrace element) throws Exception {
+    protected void execute(final MessageTrace mt) throws Exception {
+        writeMessageTrace(mt);
+        this.reportSuccess(mt.getTraceId());
+    }
 
+    private void writeMessageTrace(final MessageTrace mt) {
+        System.out.println("successfully received MessageTrace: " + mt);
     }
 }
--- Kieker2Uml/src/main/java/org/kurz/ma/examples/kieker2uml/teetime/TeeTimeConfiguration.java
+++ Kieker2Uml/src/main/java/org/kurz/ma/examples/kieker2uml/teetime/TeeTimeConfiguration.java
@@ -1,96 +1,59 @@
 package org.kurz.ma.examples.kieker2uml.teetime;
 
-import kieker.analysis.architecture.trace.TraceEventRecords2ExecutionAndMessageTraceStage;
 import kieker.analysis.architecture.trace.execution.ExecutionRecordTransformationStage;
-import kieker.analysis.architecture.trace.flow.EventRecordTraceReconstructionStage;
 import kieker.analysis.architecture.trace.reconstruction.TraceReconstructionStage;
 import kieker.analysis.generic.DynamicEventDispatcher;
 import kieker.analysis.generic.IEventMatcher;
 import kieker.analysis.generic.ImplementsEventMatcher;
-import kieker.analysis.util.debug.ListCollectionFilter;
 import kieker.common.record.controlflow.OperationExecutionRecord;
-import kieker.common.record.flow.trace.AbstractTraceEvent;
 import kieker.model.repository.SystemModelRepository;
-import kieker.model.system.model.AbstractTrace;
 import kieker.tools.source.LogsReaderCompositeStage;
 import org.kurz.ma.examples.kieker2uml.cli.CliParameters;
-import org.kurz.ma.examples.kieker2uml.stage.PrintOutputStage;
+import org.kurz.ma.examples.kieker2uml.filter.SequenceDiagrammFilter;
 import teetime.framework.Configuration;
-import teetime.framework.InputPort;
-import teetime.stage.basic.merger.Merger;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
+
 
 public class TeeTimeConfiguration extends Configuration {
 
+    private LogsReaderCompositeStage reader;
+    private SystemModelRepository systemModelRepository;
+    private TraceReconstructionStage traceReconstructionStage;
+    private ExecutionRecordTransformationStage executionRecordTransformationStage;
+    private DynamicEventDispatcher dispatcher;
+    private IEventMatcher<? extends OperationExecutionRecord> operationExecutionDispatcherOutput;
+    private SequenceDiagrammFilter sequenceDiagramFilter;
+
     public TeeTimeConfiguration(final CliParameters parameters) {
-        final SystemModelRepository systemModelRepository = new SystemModelRepository();
+        setup(parameters);
 
-        final LogsReaderCompositeStage reader = new LogsReaderCompositeStage(parameters.getInputDirectories().stream().map(Path::toFile).toList(), false, null);
-
-        final IEventMatcher<? extends AbstractTraceEvent> traceEventMatcher = new ImplementsEventMatcher<>(AbstractTraceEvent.class, null);
-        final IEventMatcher<? extends OperationExecutionRecord> operationExecutionEventMatcher = new ImplementsEventMatcher<>(OperationExecutionRecord.class, null);
-
-        final DynamicEventDispatcher dispatcher = new DynamicEventDispatcher(operationExecutionEventMatcher, true, false, false);
-
-        dispatcher.registerOutput(traceEventMatcher);
-
-        final ExecutionRecordTransformationStage executionRecordTransformationFilter = new ExecutionRecordTransformationStage(systemModelRepository);
-
-        boolean ignoreInvalidTraces = true;
-        long maxTraceDuration = Long.MAX_VALUE;
-
-        final TraceReconstructionStage traceReconstructionFilter = new TraceReconstructionStage(systemModelRepository,
-                TimeUnit.NANOSECONDS, ignoreInvalidTraces, maxTraceDuration);
-
-        boolean repairEventBasedTraces = true;
-        long maxTraceTimeout = Long.MAX_VALUE; // deactivate timeout and time input port
-
-        final EventRecordTraceReconstructionStage eventRecordTraceReconstructionFilter = new EventRecordTraceReconstructionStage(TimeUnit.NANOSECONDS,
-                repairEventBasedTraces, maxTraceDuration, maxTraceTimeout);
-
-        final boolean enhanceJavaContructors = true;
-        final boolean enhanceCallDetection = true;
-        final boolean ignoreAssumedCalls = false;
-
-        final TraceEventRecords2ExecutionAndMessageTraceStage ter2eamt = new TraceEventRecords2ExecutionAndMessageTraceStage(
-                systemModelRepository,
-                enhanceJavaContructors,
-                enhanceCallDetection,
-                ignoreAssumedCalls
-        );
-
-        final Merger<AbstractTrace> merger = new Merger<>();
-        final InputPort<AbstractTrace> traceReconstructorInputPort = merger.getNewInputPort();
-        final InputPort<AbstractTrace> ter2eamtExecutionInputPort = merger.getNewInputPort();
-        final InputPort<AbstractTrace> ter2eamtMessageInputPort = merger.getNewInputPort();
-
-        final int maxNumberOfEntries = 10000;
-        // output
-        final ListCollectionFilter<AbstractTrace> listCollectionStage = new ListCollectionFilter<>(maxNumberOfEntries,
-                ListCollectionFilter.ListFullBehavior.DROP_OLDEST);
-
-        final PrintOutputStage printOutputStage = new PrintOutputStage();
-
-
-        // configuration
         this.connectPorts(reader.getOutputPort(), dispatcher.getInputPort());
+        // configuration
+        this.connectPorts(operationExecutionDispatcherOutput.getOutputPort(), this.executionRecordTransformationStage.getInputPort());
+        this.connectPorts(this.executionRecordTransformationStage.getOutputPort(), this.traceReconstructionStage.getInputPort());
+        this.connectPorts(this.traceReconstructionStage.getMessageTraceOutputPort(), sequenceDiagramFilter.getInputPort());
 
-        // OperationExecutionRecord
-        this.connectPorts(operationExecutionEventMatcher.getOutputPort(), executionRecordTransformationFilter.getInputPort());
-        this.connectPorts(executionRecordTransformationFilter.getOutputPort(), traceReconstructionFilter.getInputPort());
-        this.connectPorts(traceReconstructionFilter.getExecutionTraceOutputPort(), traceReconstructorInputPort);
 
-        // AbstractTraceEVent
-        this.connectPorts(traceEventMatcher.getOutputPort(), eventRecordTraceReconstructionFilter.getTraceRecordsInputPort());
-        this.connectPorts(eventRecordTraceReconstructionFilter.getValidTracesOutputPort(), ter2eamt.getInputPort());
+    }
 
-        this.connectPorts(ter2eamt.getExecutionTraceOutputPort(), ter2eamtExecutionInputPort);
-        this.connectPorts(ter2eamt.getMessageTraceOutputPort(), ter2eamtMessageInputPort);
+    private void setup(final CliParameters parameters) {
+        this.systemModelRepository = new SystemModelRepository();
 
-        this.connectPorts(merger.getOutputPort(), listCollectionStage.getInputPort());
-        this.connectPorts(listCollectionStage.getOutputPort(), printOutputStage.getInputPort());
+        this.reader = new LogsReaderCompositeStage(parameters.getInputDirectories().stream().map(Path::toFile).toList(), false, null);
+
+        this.dispatcher = new DynamicEventDispatcher(null, false, true, false);
+        this.operationExecutionDispatcherOutput = new ImplementsEventMatcher<>(OperationExecutionRecord.class, null);
+        dispatcher.registerOutput(operationExecutionDispatcherOutput);
+
+        this.traceReconstructionStage = new TraceReconstructionStage(this.systemModelRepository, TimeUnit.MILLISECONDS,true, Long.MAX_VALUE);
+
+        this.executionRecordTransformationStage = new ExecutionRecordTransformationStage(this.systemModelRepository);
+        executionRecordTransformationStage.declareActive();
+
+        this.sequenceDiagramFilter = new SequenceDiagrammFilter(this.systemModelRepository, Paths.get("output.uml"));
     }
 
 }
