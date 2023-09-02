3d08eaacd225f19d2c4c3bf8e605fe69b2068189
Clemens Kurz <ClemensB.Kurz@gmail.com>
2023-09-03 00:23:15 +0200

    EclipseEcoreUml2 * Added libraries for uml2 example * Added class for uml2 example * example source: https://wiki.eclipse.org/MDT/UML2/Getting_Started_with_UML2

--- EclipseEcoreUml2/build.gradle.kts
+++ EclipseEcoreUml2/build.gradle.kts
@@ -6,20 +6,10 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven { url = uri("https://repo.eclipse.org/content/repositories/releases/") }
-    maven { url = uri("https://repo.eclipse.org/content/repositories/snapshots/") }
-    maven { url = uri("https://repo.eclipse.org/service/local/repositories/acceleo-releases/content/") }
-    maven { url = uri("https://repo.eclipse.org/service/local/repositories/maven_central/content") }
+    mavenCentral()
 }
 
 dependencies {
-    // emf
-    implementation("org.eclipse.emf:org.eclipse.emf.common:2.9.2.v20131212-0545")
-    implementation("org.eclipse.emf:org.eclipse.emf.ecore:2.9.2.v20131212-0545")
-    implementation("org.eclipse.emf:org.eclipse.emf.ecore.xmi:2.9.1.v20131212-0545")
-    implementation("org.eclipse.emf:org.eclipse.emf.mapping:2.13.0")
-
-    // uml
-    implementation("org.eclipse.uml2:uml:5.0.0-v20140602-0749")
-    implementation("org.eclipse.uml2:common:2.0.0-v20140602-0749")
+    implementation(fileTree("libs"))
+    implementation("org.slf4j:slf4j-api:1.7.30")
 }
