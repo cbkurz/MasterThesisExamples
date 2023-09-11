604712ef4f06d0fc5f0ff547b043d6e9876efc40
Clemens Kurz <ClemensB.Kurz@gmail.com>
2023-09-11 02:16:24 +0200

    Intermediate State * UML MARTE implementation * Added some bookstore traces

--- EclipseEcoreUml2/src/main/java/org/kurz/ma/examples/eclipse/ecore/uml2/UmlProfileMain.java
+++ EclipseEcoreUml2/src/main/java/org/kurz/ma/examples/eclipse/ecore/uml2/UmlProfileMain.java
@@ -1,17 +1,18 @@
 package org.kurz.ma.examples.eclipse.ecore.uml2;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.papyrus.MARTE.MARTEPackage;
-import org.eclipse.papyrus.MARTE.MARTE_AnalysisModel.GQAM.GQAMPackage;
-import org.eclipse.papyrus.MARTE.MARTE_AnalysisModel.GQAM.impl.GQAMPackageImpl;
-import org.eclipse.papyrus.MARTE.MARTE_AnalysisModel.impl.MARTE_AnalysisModelPackageImpl;
+import org.eclipse.papyrus.MARTE.MARTE_AnalysisModel.GQAM.GQAMFactory;
+import org.eclipse.papyrus.MARTE.MARTE_AnalysisModel.GQAM.GaExecHost;
 import org.eclipse.papyrus.MARTE.impl.MARTEFactoryImpl;
-import org.eclipse.papyrus.MARTE.impl.MARTEPackageImpl;
 import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.Stereotype;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.resource.UMLResource;
@@ -19,39 +20,42 @@ import org.eclipse.uml2.uml.resources.util.UMLResourcesUtil;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 public class UmlProfileMain {
 
-    private static final ResourceSet resourceSet = new ResourceSetImpl();
+    private static final ResourceSet RESOURCE_SET = new ResourceSetImpl();
 
     static {
-        UMLResourcesUtil.init(resourceSet);
+        UMLResourcesUtil.init(RESOURCE_SET);
     }
 
     public static void main(String[] args) throws IOException {
         final Model model = createModel("ModelWithMarteImport");
 
-        // Import of primitive Types (works fine)
-        importProfile(model, URI.createURI(UMLResource.UML_PRIMITIVE_TYPES_LIBRARY_URI));
+        final URI marteUri = URI.createFileURI(Paths.get("EclipseEcoreUml2").resolve("MARTE").toString()).appendSegment("MARTE").appendFileExtension(UMLResource.PROFILE_FILE_EXTENSION);
+        final Package profile = load(marteUri);
 
-        // Some MARTE init or else an exception is thrown. (only one is required, doesn't matter which one)
-        MARTEPackageImpl.init();
+        System.out.println(profile.getNestedPackages().stream().map(NamedElement::getName).collect(Collectors.joining(", ")));
+
+        importProfile(model, marteUri);
+
         MARTEFactoryImpl.init();
-        MARTE_AnalysisModelPackageImpl.init();
-        GQAMPackageImpl.init();
+        final GaExecHost gaExecHost = (GaExecHost) model.applyStereotype(getGaExecHost());
+        gaExecHost.setClockOvh("ck-clock");
 
-        // Import of MARTE namespaces. (not working)
-        // Reason: the "EcoreUtil.getObjectByType" returns null
-        importProfile(model, URI.createURI(MARTEPackage.eNS_URI));
-        importProfile(model, URI.createURI(MARTE_AnalysisModelPackageImpl.eNS_URI));
-        importProfile(model, URI.createURI(GQAMPackage.eNS_URI));
 
-        // Write model to file
+//        model.createOwnedStereotype("")
+
         save(model);
     }
 
+    private static Stereotype getGaExecHost() {
+        return (Stereotype) GQAMFactory.eINSTANCE.getGQAMPackage().getGaExecHost();
+    }
+
     protected static void importProfile(Model model, URI uri) {
-        final Resource resource = resourceSet.getResource(uri, true);
+        final Resource resource = RESOURCE_SET.getResource(uri, true);
         final org.eclipse.uml2.uml.Package umlLibrary = (org.eclipse.uml2.uml.Package) EcoreUtil.getObjectByType(resource.getContents(), UMLPackage.Literals.PACKAGE);
         model.createPackageImport(umlLibrary);
     }
@@ -66,9 +70,28 @@ public class UmlProfileMain {
         URI outputURI = URI.createFileURI(Paths.get("model-output").toString())
                 .appendSegment("ModelWithMarte")
                 .appendFileExtension(UMLResource.FILE_EXTENSION);
-        Resource resource = resourceSet.createResource(outputURI);
+        Resource resource = RESOURCE_SET.createResource(outputURI);
         resource.getContents().add(package_);
         resource.save(null);
     }
+
+    protected static org.eclipse.uml2.uml.Package load(URI uri) {
+        org.eclipse.uml2.uml.Package package_ = null;
+
+        try {
+            // Load the requested resource
+            Resource resource = RESOURCE_SET.getResource(uri, true);
+
+            // Get the first (should be only) package from it
+            package_ = (org.eclipse.uml2.uml.Package) EcoreUtil
+                    .getObjectByType(resource.getContents(),
+                            UMLPackage.Literals.PACKAGE);
+        } catch (WrappedException we) {
+            System.exit(1);
+        }
+
+        return package_;
+    }
+
 
 }
