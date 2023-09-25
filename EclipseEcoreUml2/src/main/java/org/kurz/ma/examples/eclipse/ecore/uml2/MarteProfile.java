b3b13bf40bc9381668b5204fb2e9e62ea80baad2
Clemens Kurz <ClemensB.Kurz@gmail.com>
2023-09-25 11:08:23 +0200

    EclipseEcoreUml2 * More attempts to load MARTE

--- EclipseEcoreUml2/src/main/java/org/kurz/ma/examples/eclipse/ecore/uml2/MarteProfile.java
+++ EclipseEcoreUml2/src/main/java/org/kurz/ma/examples/eclipse/ecore/uml2/MarteProfile.java
@@ -1,26 +1,24 @@
 package org.kurz.ma.examples.eclipse.ecore.uml2;
 
 import org.eclipse.emf.common.util.WrappedException;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.eclipse.uml2.uml.Profile;
-import org.eclipse.uml2.uml.UMLPackage;
 
 public class MarteProfile {
 
-    private static final Profile MARTE;
+//    private static final Profile MARTE;
 
     static {
         org.eclipse.uml2.uml.Package package_ = null;
 
         try {
             // Load the requested resource
+/*
             Resource resource = RESOURCE_SET.getResource(uri, true);
 
             // Get the first (should be only) package from it
             package_ = (org.eclipse.uml2.uml.Package) EcoreUtil
                     .getObjectByType(resource.getContents(),
                             UMLPackage.Literals.PACKAGE);
+*/
         } catch (WrappedException we) {
             System.exit(1);
         }
--- EclipseEcoreUml2/src/main/java/org/kurz/ma/examples/eclipse/ecore/uml2/UmlProfileMain.java
+++ EclipseEcoreUml2/src/main/java/org/kurz/ma/examples/eclipse/ecore/uml2/UmlProfileMain.java
@@ -2,16 +2,19 @@ package org.kurz.ma.examples.eclipse.ecore.uml2;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.common.util.WrappedException;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.papyrus.MARTE.MARTE_AnalysisModel.GQAM.GQAMFactory;
 import org.eclipse.papyrus.MARTE.MARTE_AnalysisModel.GQAM.GaExecHost;
-import org.eclipse.papyrus.MARTE.impl.MARTEFactoryImpl;
+import org.eclipse.uml2.uml.Device;
+import org.eclipse.uml2.uml.Interaction;
+import org.eclipse.uml2.uml.Lifeline;
 import org.eclipse.uml2.uml.Model;
-import org.eclipse.uml2.uml.NamedElement;
-import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.PackageImport;
+import org.eclipse.uml2.uml.Profile;
 import org.eclipse.uml2.uml.Stereotype;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UMLPackage;
@@ -20,7 +23,7 @@ import org.eclipse.uml2.uml.resources.util.UMLResourcesUtil;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
+import java.util.List;
 
 public class UmlProfileMain {
 
@@ -34,15 +37,18 @@ public class UmlProfileMain {
         final Model model = createModel("ModelWithMarteImport");
 
         final URI marteUri = URI.createFileURI(Paths.get("EclipseEcoreUml2").resolve("MARTE").toString()).appendSegment("MARTE").appendFileExtension(UMLResource.PROFILE_FILE_EXTENSION);
-        final Package profile = load(marteUri);
 
-        System.out.println(profile.getNestedPackages().stream().map(NamedElement::getName).collect(Collectors.joining(", ")));
+        final PackageImport packageImport = importProfile(model, marteUri);
 
-        importProfile(model, marteUri);
+        final Profile profile = (Profile) packageImport.getImportedPackage();
+        createNodes(model, profile);
 
-        MARTEFactoryImpl.init();
-        final GaExecHost gaExecHost = (GaExecHost) model.applyStereotype(getGaExecHost());
-        gaExecHost.setClockOvh("ck-clock");
+        final Device device = UMLFactory.eINSTANCE.createDevice();
+        final GaExecHost stereotype = (GaExecHost) device.applyStereotype(getGaExecHost());
+
+
+//        final GaExecHost gaExecHost = (GaExecHost) model.applyStereotype(getGaExecHost());
+//        gaExecHost.setClockOvh("ck-clock");
 
 
 //        model.createOwnedStereotype("")
@@ -50,14 +56,23 @@ public class UmlProfileMain {
         save(model);
     }
 
+    private static void createNodes(final Model model, final Profile profile) {
+        final EClass interactionEClass = UMLFactory.eINSTANCE.createInteraction().eClass();
+        final Interaction interaction = (Interaction) model.createPackagedElement("MyInteraction", interactionEClass);
+        final Lifeline lifeline = interaction.createLifeline("MyLifeline");
+        final List<Stereotype> list = profile.allApplicableStereotypes().stream().filter(interaction::isStereotypeApplicable).toList();
+        System.out.println("Applicable Stereotypes: " + list);
+    }
+
     private static Stereotype getGaExecHost() {
         return (Stereotype) GQAMFactory.eINSTANCE.getGQAMPackage().getGaExecHost();
     }
 
-    protected static void importProfile(Model model, URI uri) {
+    protected static PackageImport importProfile(Model model, URI uri) {
         final Resource resource = RESOURCE_SET.getResource(uri, true);
         final org.eclipse.uml2.uml.Package umlLibrary = (org.eclipse.uml2.uml.Package) EcoreUtil.getObjectByType(resource.getContents(), UMLPackage.Literals.PACKAGE);
-        model.createPackageImport(umlLibrary);
+        final PackageImport packageImport = model.createPackageImport(umlLibrary);
+        return packageImport;
     }
 
     protected static Model createModel(String name) {
