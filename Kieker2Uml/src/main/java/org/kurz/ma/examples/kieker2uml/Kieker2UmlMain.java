952abfa48d81522062624d88e39ba20f57ddb72c
Clemens Kurz <ClemensB.Kurz@gmail.com>
2023-11-26 00:42:02 +0100

    Kieker2Uml * removed writing logging to a file

--- Kieker2Uml/src/main/java/org/kurz/ma/examples/kieker2uml/Kieker2UmlMain.java
+++ Kieker2Uml/src/main/java/org/kurz/ma/examples/kieker2uml/Kieker2UmlMain.java
@@ -1,12 +1,8 @@
 package org.kurz.ma.examples.kieker2uml;
 
-import org.apache.commons.io.FileUtils;
 import org.kurz.ma.examples.kieker2uml.teetime.Kieker2UmlTeeTimeService;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 
 public class Kieker2UmlMain {
 
@@ -14,8 +10,6 @@ public class Kieker2UmlMain {
     }
 
     public static void main(final String[] args) throws IOException {
-        final File file = FileUtils.getFile("output.txt");
-        FileUtils.write(file, "start at " + Instant.now(), StandardCharsets.UTF_8, false);
         final Kieker2UmlTeeTimeService service = Kieker2UmlTeeTimeService.getInstance();
         System.exit(service.run("Kieker2Uml", "kieker-2-uml", args, service.getParameters()));
     }
