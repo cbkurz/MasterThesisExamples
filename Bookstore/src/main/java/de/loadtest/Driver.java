744ecd7d24ab1e7fde6e8c6e30cf02096b01d836
Clemens Kurz <ClemensB.Kurz@gmail.com>
2023-12-25 17:20:31 +0100

    Cocome * added example how the cocome lqn output looks like

--- Bookstore/src/main/java/de/loadtest/Driver.java
+++ Bookstore/src/main/java/de/loadtest/Driver.java
@@ -8,14 +8,16 @@ import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
+    private static int counter = 0;
+
     public static void main(String[] args) {
-        final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(20);
+        final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
         scheduledExecutorService.scheduleAtFixedRate(Driver::bookstoreExecution, 0, 2, TimeUnit.SECONDS);
         scheduledExecutorService.schedule(scheduledExecutorService::shutdown, 20, TimeUnit.SECONDS);
     }
 
     private static void bookstoreExecution() {
-        System.out.println("Starting the Bookstore");
+        System.out.println("Starting the Bookstore: " + counter++);
         Main.main(new String[0]);
     }
 }
